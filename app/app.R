d158f5298d1d13330a3802578e93acba04f2de0a
Tom Weishaar <tom.weishaar@gmail.com>
2017-09-21 10:55:52 -0400

    v1.3 Improved console messaging
    To better understand what's going on inside Shiny with this technique.

--- app/app.R
+++ app/app.R
@@ -1,10 +1,11 @@
 ### uiStub
-### v1.2 - Tom Weishaar - Sep 2017
+### v1.3 - Tom Weishaar - Sep 2017
 
 ### v1.1 fixes a security issue
 ###    for details, see: https://groups.google.com/forum/?utm_source=digest&utm_medium=email#!topic/shiny-discuss/zxoLK1kKTCw)
 ### v1.2 adds some tagList() functions that aren't strictly required here, but make it
 ###    much easier to expand this code.
+### v1.3 added more informative console messages.
 
 ### Simple demo program that "moves" the ui into the server, thereby allowing the use of
 ###    the URL "search" protocol to create multi-page web sites. The advantage is that
@@ -16,36 +17,40 @@
 
 library(shiny)
 
+# put a message in console or server log; note this happens only when the app is started!
+cat("uiStub application started...\n")
+
 ui <- uiOutput("uiStub")                                # single-output stub ui
 
-print("uiStub application loaded...")                   # note this prints only once... (in
-                                                        #    console locally or in server log)
 server <- function(input, output, session) {
+   cat("Session started.\n")                               # this prints when a session starts
+   onSessionEnded(function() {cat("Session ended.\n\n")})  # this prints when a session ends
 
    # build menu; same on all pages
-   output$uiStub <- renderUI(tagList(                   # a single-output stub ui basically lets you
-      fluidPage(                                        #     move the ui into the server function
+   output$uiStub <- renderUI(tagList(             # a single-output stub ui basically lets you
+      fluidPage(                                  #     move the ui into the server function
          fluidRow(
             column(12,
                HTML("<h3><a href='?home'>Home</a> | ",
-                    "<a href='?oldFaithful'>Old Faithful</a> |",
+                    "<a href='?old-faithful'>Old Faithful</a> |",
                     "<a href='?page3'>Nothing</a>",
                     "</h3>")
                )
             ),
-         uiOutput("pageStub")                              # loaded server code should render the
-      )
-   ))                                                   #    rest of the page to this output$
+         uiOutput("pageStub")                     # loaded server code should render the
+      )                                           #    rest of the page to this output$
+   ))
 
    # load server code for page specified in URL
-   validFiles = c("home.R", "oldFaithful.R")            # valid files must be hardcoded here
-                                                        #    for security
+   validFiles = c("home.R",                             # valid files must be hardcoded here
+                  "old-faithful.R")                     #    for security (use all lower-case
+                                                        #    names to prevent Unix case problems)
    fname = isolate(session$clientData$url_search)       # isolate() deals with reactive context
-
-   print(paste0("New session, filename: ", fname))      # note this prints for each new URL...
-                                                        #    (a new URL gets a new server() function)
    if(nchar(fname)==0) { fname = "?home" }              # blank means home page
    fname = paste0(substr(fname, 2, nchar(fname)), ".R") # remove leading "?", add ".R"
+
+   cat(paste0("Session filename: ", fname, ".\n"))      # print the URL for this session
+
    if(!fname %in% validFiles){                          # is that one of our files?
       output$pageStub <- renderUI(tagList(              # 404 if no file with that name
          fluidRow(
@@ -55,8 +60,8 @@ server <- function(input, output, session) {
             )
          )
       ))
-      return()                                          # prevents a "file not found" error on
-   }                                                    #    the next line after a 404 error
+      return()    # to prevent a "file not found" error on the next line after a 404 error
+   }
    source(fname, local=TRUE)                            # load and run server code for this page
 }
 # Run the application
